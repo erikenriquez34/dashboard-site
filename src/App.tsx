48e9ea9fe0bb1593b8265c4f8e7910fa202ad716
Erik Enriquez <erikenriquez34@gmail.com>
2025-05-25 14:39:00 -0600

    Navbar and about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-      <h1 className="text-2xl font-bold text-blue-600">Hello World</h1>
-      <p className="mt-2 text-gray-700">Styled text and layout, everything looks good!</p>
-        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer">
-          Click Me
-        </button>
-    </>
+    <div className="">
+      <Navbar/>
+      <main className="min-h-[80vh] xl:my-28 mt-36 pt-16">
+        <section className="flex flex-row items-center justify-between px-24">
+          <div className="px-24">
+            <h1 className="text-6xl font-bold">Hello I'm</h1>
+            <h1 className="text-6xl mb-6 font-bold text-amber-500">Erik Enriquez</h1>
+            <p className="text-xl"> Currently a student, I’m especially intrested in GPUs, systems programming, and 
+              performance engineering. I like working with Linux, and like enjoy coding in
+              Java and C++. </p>
+          </div>
+
+          <div className="px-24">
+            <img src="../public/simulate-guy.png" width="1000vh" height="1000vh"/>
+          </div>
+ 
+        </section>
+      </main>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+function Navbar() {
+  return (
+    <nav className="bg-transparent border-gray-200 fixed w-full z-50 p-3.5 backdrop-blur-md shadow-xl top-0 left-0">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          <img src="../../public/body.png" width="50vh" height="50vh" className="cursor-pointer" />
+
+          <div className="space-x-6 hidden md:flex">
+            <a href="#about" className="text-white font-bold hover:text-amber-500 transition">
+              About
+            </a>
+            <a href="#projects" className="text-white font-bold hover:text-amber-500 transition">
+              Projects
+            </a>
+            <a href="#media" className="text-white font-bold hover:text-amber-500 transition">
+              Media
+            </a>
+            <a href="#contact" className="text-white font-bold hover:text-amber-500 transition">
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

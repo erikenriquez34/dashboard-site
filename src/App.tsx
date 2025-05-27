916137aa59c9838194e64a050109fef18d1e07ed
Erik Enriquez <erikenriquez34@gmail.com>
2025-05-26 19:16:06 -0600

    Added my links

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,31 @@ function App() {
           <div className="px-24">
             <h1 className="text-6xl font-bold">Hello I'm</h1>
             <h1 className="text-6xl mb-6 font-bold text-amber-500">Erik Enriquez</h1>
-            <p className="text-xl"> Currently a student, I’m especially intrested in GPUs, systems programming, and 
+            <p className="text-xl mb-9"> Currently a student, I’m especially intrested in GPUs, systems programming, and 
               performance engineering. I like working with Linux, and like enjoy coding in
               Java and C++. </p>
+            <div className="flex gap-4">
+              <a className="bg-gray-50 w-10 h-10 rounded-4xl cursor-pointer flex items-center justify-center hover:scale-110" 
+              href="https://github.com/erikenriquez34" target="_blank">
+                <img src="github.png" width="25vh" height="25vh"/>
+              </a>
+              <a className="bg-gray-50 w-10 h-10 rounded-4xl cursor-pointer flex items-center justify-center hover:scale-110"
+              href="https://www.linkedin.com/in/erikenriquez34/" target="_blank">
+                <img src="linkedin.png" width="25vh" height="25vh"/>
+              </a>
+              <a className="bg-gray-50 w-10 h-10 rounded-4xl cursor-pointer flex items-center justify-center hover:scale-110"
+              href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=erikenriquez34@gmail.com" target="_blank">
+                <img src="gmail.png" width="25vh" height="25vh"/>
+              </a>
+              <a className="bg-gray-50 w-10 h-10 rounded-4xl cursor-pointer flex items-center justify-center hover:scale-110"
+              href="https://leetcode.com/u/erikenriquez34/" target="_blank">
+                <img src="leetcode.png" width="25vh" height="25vh"/>
+              </a>
+            </div>
           </div>
 
           <div className="px-24">
-            <img src="../public/simulate-guy.png" width="1000vh" height="1000vh"/>
+            <img src="../public/simulate-guy.png" className="mb-9" width="1250vh" height="1250vh"/>
           </div>
  
         </section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,9 @@ function Navbar() {
     <nav className="bg-transparent border-gray-200 fixed w-full z-50 p-3.5 backdrop-blur-md shadow-xl top-0 left-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
-          <img src="../../public/body.png" width="50vh" height="50vh" className="cursor-pointer" />
+          <div className="cursor-pointer">
+            <img src="../../public/body.png" width="50vh" height="50vh" />
+          </div>
 
           <div className="space-x-6 hidden md:flex">
             <a href="#about" className="text-white font-bold hover:text-amber-500 transition">
